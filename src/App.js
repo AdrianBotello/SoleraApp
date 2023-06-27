d19f5e21d0368399a610145f113600de0ea0f18c
isvakuSolera <137295258+isvakuSolera@users.noreply.github.com>
2023-06-27 09:30:41 +0200

    vers 1.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+//import tailwind
+import './App.css';
 import * as React from "react";
-import * as ReactDOM from "react-dom/client";
+//router import
 import {
   createBrowserRouter,
   RouterProvider,
 }
-  from "react-router-dom";
-import './App.css';
+from "react-router-dom";
+//import others html pages
 import Mainpage from './html/mainpage'
 
+//this part is necessary to make the router, to be able to access any page
 const router = createBrowserRouter([
   {
+    //default page
     path: "/",
     element: <Mainpage />,
   },
 ]);
 
-
+//in this part add the router
 function App() {
   return (
     <React.StrictMode>
