9ea4bc1fab6ed0799e4fee19e6f4e3e2bcc1686d
LucasAMSolera <lucas.munoz@solera.com>
2023-06-27 08:49:23 +0200

    New Database location
    The server folder location has been changed in order for the webapp to access it

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,3 +1,8 @@
+using MongoDB.Driver;
+using System.Diagnostics;
+
+mongoInitializer();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +28,19 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void mongoInitializer()
+{
+    var mongo = Process.Start("Server/server.bat");
+
+    Task.Delay(2000).Wait();
+    MongoClient dbClient = new MongoClient("mongodb://localhost:27017");
+
+    var dbList = dbClient.ListDatabases().ToList();
+
+    Console.WriteLine("The list of databases on this server is: ");
+    foreach (var db in dbList)
+    {
+        Console.WriteLine(db);
+    }
+}
