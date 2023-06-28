ac66e6c3ce77cbd35f7c780263abc0440ee670e4
PabloVGA <pablo.villa@Solera.com>
2023-06-28 12:41:48 +0200

    Server.bat hotfix

--- webapi/Server/server.bat
+++ webapi/Server/server.bat
@@ -4,4 +4,4 @@ title This is the server master. Love your server master.
 
 echo Initializing server!
 taskkill /f /im mongod.exe
-start /d "%ProgramFiles%\MongoDB\Server\6.0\bin" mongod.exe --dbpath %~dp06.0\data\db --logpath %~dp06.0\log\mongod.log
+start /d "%ProgramFiles%\MongoDB\Server\6.0\bin" mongod.exe --dbpath "%~dp06.0\data\db" --logpath "%~dp06.0\log\mongod.log"
